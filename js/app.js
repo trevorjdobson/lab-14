02991de6676c027d5636d602cc470004ae83b64d
trevorjdobson <trevorjdobson@gmail.com>
2019-06-20 13:16:24 -0700

    added item from demo

--- js/app.js
+++ js/app.js
@@ -7,11 +7,14 @@ var Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // TODONE: Fill in this instance method to create a new CartItem and add it to this.items
+  var newItem = new CartItem(product, quantity);
+  this.items.push(newItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  
 };
 
 Cart.prototype.removeItem = function(item) {
